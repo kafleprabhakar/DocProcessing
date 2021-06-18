334d3fc84150d6b03d6b9f587ba9eb66de0363ec
kafleprabhakar <pkafleprabhakar@gmail.com>
2021-06-18 16:18:55 -0400

    web app with checkbox html support

--- server/static/index.js
+++ server/static/index.js
@@ -1,8 +1,43 @@
 const formElement = document.getElementById('upload-form');
 const responseContainer = document.getElementById('response-container');
 const fileInput = document.getElementById('document');
+const htmlForm = document.getElementById('parsed-form');
 
 formElement.addEventListener('submit', (e) => handleFormSubmit(e));
+
+function makeClusterElement() {
+  const cluster = document.createElement('div');
+  cluster.classList.add("cluster", "py-3", "px-2", "border", "rounded", "my-3");
+  return cluster
+}
+
+function makeCheckbox(label, status) {
+  const checkbox = document.getElementById('checkbox-template').content.cloneNode(true);
+  const inputLabel = checkbox.querySelector('.form-check-label');
+  const input = checkbox.querySelector('.form-check-input');
+  // console.log(checkbox);
+  input.checked = status;
+  inputLabel.textContent = label;
+  const randomId = 'checkbox_' + Math.random().toString(36).substring(7);
+  input.setAttribute('id', randomId);
+  inputLabel.setAttribute('for', randomId);
+  // label.for = randomId;
+  return checkbox;
+}
+
+function makeHTMLForm(data) {
+  htmlForm.innerHTML = '';
+  for (var cluster of data) {
+    // console.log(cluster);
+    // const clusterElement = document.createElement('div');
+    // const clusterElement = document.getElementById('cluster-template').content.cloneNode(true);
+    const clusterElement = makeClusterElement();
+    for (var element of cluster) {
+      clusterElement.appendChild(makeCheckbox(element.label, element.percent_filled > 0.15));
+    }
+    htmlForm.appendChild(clusterElement);
+  }
+}
 
 function handleFormSubmit(e) {
   e.preventDefault();
@@ -16,9 +51,8 @@ function handleFormSubmit(e) {
     body: toSend,
   }).then(response => response.json())
     .then(data => {
-      responseContainer.innerHTML = JSON.stringify(data, null, 2);
-      responseContainer.classList.remove("prettyprinted");
-      console.log(data);
+      // responseContainer.innerHTML = JSON.stringify(data, null, 2);
+      makeHTMLForm(data);
       PR.prettyPrint();
     });
 }
